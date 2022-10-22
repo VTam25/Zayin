40542f800e1ca22531d5c357699f2e9a2c9ff227
louie7679 <louie7679@gmail.com>
2022-10-21 23:38:01 -0400

    Banner CSS

--- client/main.js
+++ client/main.js
@@ -1,5 +1,12 @@
-document.getElementById('file').addEventListener("change", (event) => {
+// document.getElementById('file').addEventListener("change", (event) => {
+//     const image = document.getElementById("output");
+//     image.src = URL.createObjectURL(event.target.files[0]);
+// });
+
+document.getElementById('file').addEventListener("change", loadFile);
+
+function loadFile(event) {
     const image = document.getElementById("output");
     image.src = URL.createObjectURL(event.target.files[0]);
-});
+};
 
