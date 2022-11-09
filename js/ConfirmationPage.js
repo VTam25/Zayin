1819b9aee54424e6dd6fae58eecfe32afedfc0c5
louie7679 <louie7679@gmail.com>
2022-11-08 23:20:30 -0500

    added confirmation page button functionality

---
+++ js/ConfirmationPage.js
@@ -0,0 +1,7 @@
+document.getElementById('yes').addEventListener("click", (event) => 
+    location.href = "movie.html"
+);
+
+document.getElementById('no').addEventListener("click", (event) => 
+    location.href = "dashboard.html"
+);
