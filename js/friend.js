e9135f86b6476d0b44bd0b875012bb386201d444
Vivienne Tam <vivienne.tam2020@gmail.com>
2022-11-09 23:38:07 -0500

    Friend CRUD operations

--- js/friend.js
+++ js/friend.js
@@ -17,14 +17,19 @@ function reload(filename) {
     }
 }
 
+function showList(response) {
+    // TODO
+    response.write("<h1> Current Friends: </h1>")
+    response.write(JSON.stringify(friend));
+    response.end();
+}
+
 function addFriend(name, response) {
     friend[name] = "No Movie List";
 
     // TODO
     fs.writeFileSync(JSONfile, JSON.stringify(friend));
-    response.write("<h1> Friend " + name + " added </h1>");
-    response.write("<h1> Current Friends: </h1>")
-    response.write(JSON.stringify(friend));
+    response.write("<h1> Friend created </h1>");
     response.end();
 }
  
@@ -35,10 +40,14 @@ let server = http.createServer();
 server.on('request', async (request, response) => {
     response.writeHead(200, headerText);
     let options = url.parse(request.url, true).query;
-    response.write(JSON.stringify(options));
+    //response.write(JSON.stringify(options));
 
     if (request.url.startsWith("/add")) {
         addFriend(options.name, response);
+        return;
+    }
+    if (request.url.startsWith("/show")) {
+        showList(response);
         return;
     }
     else {
@@ -53,5 +62,4 @@ server.listen(8080);
 //     console.log("event");
 //     const newFriend = document.getElementById("friend_name").value;
 //     const response = fetch('http://localhost:8000/friends/');
-//     console.log(response);
 // });
