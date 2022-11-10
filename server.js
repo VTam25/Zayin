50a30f688514ed59703bbf7d8352304ff842240d
Foram Patel <forampatel@vl965-172-31-118-36.wireless.umass.edu>
2022-11-09 23:39:29 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,44 +1,25 @@
-const express = require('express'); // express itself w/ CommonJS
-const app = express(); // this is the "app"
+const express = require('express'); 
+const app = express(); 
 app.use(express.static('Zayin'));
-const port = 8000;     // we will listen on this port
+const port = 8000;    
 
-app.get('/login/', (req, res) => {  // when you go to the root, hello!
-  console.log("inside");
+app.get('/login/', (req, res) => {  
   response = {
     first_name: req.query.first_name,
     last_name: req.query.last_name
  };
  console.log(response);
  res.send(JSON.stringify(response));
-  //res.sendFile('../index.html');
 });
 
-// app.post('/login/', (req, res) => {  // when you go to the root, hello!
-//   console.log("inside");
-//   response = {
-//     first_name:req.query.username,
-//     last_name:req.query.password
-//  };
-//  console.log(response);
-//  res.send(JSON.stringify(response));
-//   //res.sendFile('../index.html');
-// });
-
-
-// app.post('/login/', function (req, res) {
-//   // Prepare output in JSON format
-//   console.log("inside");
-//   response = {
-//      first_name:req.body.username,
-//      last_name:req.body.password
-//   };
-//   console.log(response);
-//   res.send(JSON.stringify(response));
-// })
-
-//app.use('/', express.static('index.html'));
-
+app.post('/login/', (req, res) => {  
+  response = {
+    first_name:req.query.username,
+    last_name:req.query.password
+ };
+ console.log(response);
+ res.send(JSON.stringify(response));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
