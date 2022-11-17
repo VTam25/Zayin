f7b95b671a2faeb72500bfdd45c9345fd066efc3
Vivienne Tam <vivienne.tam2020@gmail.com>
2022-11-17 14:06:23 -0500

    Stuff we need for postgres

--- server.js
+++ server.js
@@ -3,6 +3,26 @@ const app = express();
 app.use(express.static("public"));
 const port = 8000;    
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
 
 app.use('/', express.static('public/html'));
 // app.use(express.static('css'));
