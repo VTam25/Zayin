e231f833c447d293dd24d37db63367d96b170ff7
Foram Patel <forampatel@vl965-172-31-118-36.wireless.umass.edu>
2022-11-20 13:16:29 -0500

    making all pick friend buttons navigate

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,3 +1,11 @@
-document.getElementById("pick-friend").addEventListener("click", (event) => 
+document.getElementById("pick-friend1").addEventListener("click", (event) => 
     location.href = "../html/movie.html"
 );
+
+document.getElementById("pick-friend2").addEventListener("click", (event) => 
+    location.href = "../html/movie.html"
+);
+
+document.getElementById("pick-friend3").addEventListener("click", (event) => 
+    location.href = "../html/movie.html"
+); 
