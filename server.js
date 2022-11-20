94a487fa89b3628c541d52427cd9bddabcf36da0
Vivienne Tam <vivienne.tam2020@gmail.com>
2022-11-20 16:23:08 -0500

    Add a user post method

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express'); 
+const bodyParser = require('body-parser');
 const app = express(); 
 app.use(express.static("public"));
-const port = 8000;    
+const port = 8000;   
+
+let curr_user = "";
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', express.static('public/html'));
 
@@ -9,28 +14,31 @@ const { MongoClient } = require("mongodb");
 const uri = "mongodb+srv://team:FOQvCBE0VEC81Fbv@zayin-east.79pggjl.mongodb.net/zayin-db?retryWrites=true&w=majority"; 
 //maybe need to hide this with secrets or get the line below to work
 //const uri = process.env.MONGODB_URI;
+let database = "";
+let collection = "";
 
+MongoClient.connect(uri, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database');
+    database = client.db('zayin-db');
+    collection = database.collection('users');
+  }).catch(console.error);
 
 app.get("/friends", async function (req, res){
-  const client = new MongoClient(uri, { useUnifiedTopology: true });
-  
-  try {
-    await client.connect();
-
-    const database = client.db('zayin-db');
-    const collection = database.collection('users');
-
-    const user = await collection.find({"username": "Viv"}).toArray();
-    console.log(user[0].friends);
-    return res.json(user);
-  } catch(err) {
-    console.log(err);
-  }
-  finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+  console.log("frieeenddd");
+  const user = await collection.find({"username": "Viv"}).toArray();
+  return res.json(user);
 });
+
+app.post('/signup', async function (req, res){
+  console.log(req.body);
+  collection.insertOne(req.body).then(result => {
+    console.log(result);
+    console.log(req.body.username);
+    curr_user = req.body.username;
+  }).catch(error => console.error(error));
+  res.redirect('/AccountSetting.html');
+})
 
 // app.use(express.static('css'));
 
